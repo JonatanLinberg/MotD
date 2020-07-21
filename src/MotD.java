f952727c0baa917eae3ebec424f9a288c19fe132
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 23:40:00 +0200

    0.0.3 - pre-testing finished

--- src/MotD.java
+++ src/MotD.java
@@ -1,7 +1,10 @@
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,7 +14,9 @@ import net.md_5.bungee.api.ChatColor;
 
 public class MotD extends JavaPlugin implements Listener, CommandExecutor {
 	
-	private final String chatPrefix = ChatColor.AQUA + (ChatColor.BOLD + "[MotD] - ");
+	private final String chatPrefix = ChatColor.AQUA + (ChatColor.BOLD + "[MotD] - " + ChatColor.RESET);
+	private FileConfiguration config = getConfig();
+	private Logger log = getLogger();
 	
 	@Override
     public void onEnable() {
@@ -22,33 +27,40 @@ public class MotD extends JavaPlugin implements Listener, CommandExecutor {
 	}
     
     private void setup() {
+    	config.addDefault("motd", "This is the default MotD, change this using the command \"/motd set [MotD]\" as a server operator.");
+        config.options().copyDefaults(true);
+        saveConfig();
+    	log.info("Current MotD: " + config.getString("motd"));
     	
     }
 
 	// Fired when plugin is disabled
     @Override
     public void onDisable() {
-    	
+    	this.saveConfig();
     }
     
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-    	
-    	
-    	e.getPlayer().sendMessage("");
+    	e.getPlayer().sendMessage(chatPrefix + config.getString("motd"));
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length > 0) {
 			if (args[0].equals("set")) {
-				
-				sender.sendMessage("");
-				this.getLogger().info("MotD set to: ");
+				String motd = "";
+				for (int i = 0; i < args.length; i++) {
+					if (i > 0)
+						motd += ' ';
+					motd += args[i];
+				}
+				config.set("motd", motd);
+				sender.sendMessage(chatPrefix + "");
+				log.info("MotD set to: " + config.getString("motd"));
 				return true;
 			} else if (args[0].equals("get")) {
-				
-				sender.sendMessage("");
+				sender.sendMessage(chatPrefix + config.getString("motd"));
 				return true;
 			}
 		}
