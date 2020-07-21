d86e0dd454591f9555910107f3696021327c4b42
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:50:32 +0200

    0.0.1 - Basic command setup

--- src/MotD.java
+++ src/MotD.java
@@ -1,15 +1,24 @@
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MotD extends JavaPlugin {
+public class MotD extends JavaPlugin implements CommandExecutor {
 	
 	@Override
     public void onEnable() {
-		
+		this.getCommand("motd").setExecutor(this);
 	}
     
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
     	
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+    	return false;
     }
 }
