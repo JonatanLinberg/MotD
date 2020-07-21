d92e1d6b11bf9b08b6f5ab7b8f1f1b4d3dd4f878
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:41:14 +0200

    Added plugin main class and removed unnecessary eclipse file

---
+++ src/MotD.java
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MotD extends JavaPlugin {
+	
+	@Override
+    public void onEnable() {
+		
+	}
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    	
+    }
+}
